4ba390b5a82d97da05ccccda6074d787d619593e
Jerry Lohm <94410870+FunlightStudios@users.noreply.github.com>
2024-12-31 14:10:19 +0100

    Update run.py

--- run.py
+++ run.py
@@ -3,4 +3,4 @@ from swisseat import create_app
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(host=92.113.145.121,port=80, debug=True)
+    app.run(host=92.113.145.121, port=80, debug=True)
